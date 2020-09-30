5fa0aabe4974a40e0ad12735067c56b1e887236e
My Name <vadim.pechenin2015@gmail.com>
2020-09-30 23:50:07 +0300

    First commit. Html with thymeleaf

---
+++ src/main/java/com/example/Spring_hello_world/ServingWebContentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServingWebContentApplication.class, args);
+    }
+
+}
